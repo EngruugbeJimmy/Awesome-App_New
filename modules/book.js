22087945aef92161786a6e074a4f31db3f1b344a
abahuugbede_dev <89143961+EngruugbeJimmy@users.noreply.github.com>
2023-08-18 12:09:10 +0100

    linters disable fix

--- modules/book.js
+++ modules/book.js
@@ -1,6 +1,5 @@
 // Class book, to define a book
-// eslint-disable-next-line max-classes-per-file
-export class Book {
+export default class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
